49fba267f7e1130b1eac416d8d400fa119a54344
Julian Glattki <juliang98@web.de>
2021-12-11 20:03:21 +0100

    11

---
+++ 11/11_02.py
@@ -0,0 +1,74 @@
+def is_in_bounds(row, column):
+    if 0 <= row < len(matrix):
+        if 0 <= column < len(matrix[row]):
+            return True
+    return False
+
+
+def inc_and_mark(row, column):
+    matrix[row][column] = matrix[row][column] + 1
+    if matrix[row][column] > 9:
+        matrix[row][column] = 0
+        return True
+    return False
+
+
+def flash(row, column):
+    global FLASH_COUNT
+    FLASH_COUNT = FLASH_COUNT + 1
+
+    if is_in_bounds(row + 1, column) and 1 <= matrix[row + 1][column] < 10:
+        if inc_and_mark(row + 1, column):
+            flash(row + 1, column)
+    if is_in_bounds(row - 1, column) and 1 <= matrix[row - 1][column] < 10:
+        if inc_and_mark(row - 1, column):
+            flash(row - 1, column)
+    if is_in_bounds(row, column + 1) and 1 <= matrix[row][column + 1] < 10:
+        if inc_and_mark(row, column + 1):
+            flash(row, column + 1)
+    if is_in_bounds(row, column - 1) and 1 <= matrix[row][column - 1] < 10:
+        if inc_and_mark(row, column - 1):
+            flash(row, column - 1)
+    if is_in_bounds(row + 1, column + 1) and 1 <= matrix[row + 1][column + 1] < 10:
+        if inc_and_mark(row + 1, column + 1):
+            flash(row + 1, column + 1)
+    if is_in_bounds(row - 1, column + 1) and 1 <= matrix[row - 1][column + 1] < 10:
+        if inc_and_mark(row - 1, column + 1):
+            flash(row - 1, column + 1)
+    if is_in_bounds(row + 1, column - 1) and 1 <= matrix[row + 1][column - 1] < 10:
+        if inc_and_mark(row + 1, column - 1):
+            flash(row + 1, column - 1)
+    if is_in_bounds(row - 1, column - 1) and 1 <= matrix[row - 1][column - 1] < 10:
+        if inc_and_mark(row - 1, column - 1):
+            flash(row - 1, column - 1)
+
+
+def all_flashed():
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] != 0:
+                return False
+    return True
+
+FLASH_COUNT = 0
+file1 = open('input.txt', 'r')
+matrix = [list(map(int, list(line.strip('\n')))) for line in file1.readlines()]
+
+
+count = 0
+found = False
+
+while not found:
+    count = count + 1
+    matrix = [[column + 1 for column in row] for row in matrix]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] > 9:
+                matrix[i][j] = 0
+                flash(i, j)
+
+    if all_flashed():
+        found = True
+
+
+print(count)
